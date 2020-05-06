f67a26d13d1e2ef1012412a7043388e0667c58d8
max <maximepouzol@gmail.com>
2020-05-06 20:12:49 +0200

    correction spanshort

--- module08/ex01/span.cpp
+++ module08/ex01/span.cpp
@@ -43,7 +43,27 @@ unsigned int Span::shortestSpan()
         throw "unknow cause Container is so small to make analyze, container lenght equal 1 or ";
     std::vector<unsigned int> cpy = n;
     std::sort(cpy.begin(), cpy.end());
-    return (cpy[1] - cpy[0]);    
+    size_t i = 0;
+    size_t s = 0;
+    size_t a = 0;
+    size_t b = 0;
+    int total = 1999;
+    while (i < cpy.size())
+    {
+        s = 0;
+        while (s < cpy.size())
+        {
+            if (i != s && total > cpy[s] - cpy[i] && 0 < cpy[s] - cpy[i])
+            {
+                a = s;
+                b = i;
+                total = cpy[a] - cpy[b];
+            }
+            s++;
+        }
+        i++;
+    }
+    return (cpy[a] - cpy[b]);
     }
     catch(const char *str)
     {
